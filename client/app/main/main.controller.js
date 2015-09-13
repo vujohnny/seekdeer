bd7e9884424561936081adf18cfe3eadeddd80c1
Edgar Matevosyan <edgarmatevosyan@gmail.com>
2015-09-12 23:03:24 -0700

    got expedia call to work

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -94,44 +94,62 @@ angular.module('seekdeerApp')
 
         // End accordion functions ======================================================
 
-						// google maps
-						$scope.map = { 
-							center: { latitude: 45, longitude: -73 }, 
-							zoom: 8,
-							options: {
-							    draggable: false,
-							    scrollwheel: false,
-							    disableDefaultUI: true,
-							    mapTypeId: google.maps.MapTypeId.TERRAIN
-							}
-						};
-												
-						$scope.map.markers = [
-						{
-							id : 1,
-							latitude: 43.67023,
-							longitude: -79.38676
-					    },
-					    {
-							id : 2,
-							latitude: 43.67023,
-							longitude: -81.38676
-					    },
-					    {
-							id : 3,
-							latitude: 43.67023,
-							longitude: -80.38676
-					    }
-					    ];
-					    
-					    uiGmapGoogleMapApi.then(function(maps) {
-							var input = document.getElementById('searchTextField');
-							var options = {types: ['(cities)']};
-							var map = $scope.map;
-						
-							var autocomplete = new google.maps.places.Autocomplete(input, options);	
-					    });
-					
+		// google maps
+		$scope.map = { 
+			center: { latitude: 45, longitude: -73 }, 
+			zoom: 8,
+			options: {
+			    draggable: false,
+			    scrollwheel: false,
+			    disableDefaultUI: true,
+			    mapTypeId: google.maps.MapTypeId.TERRAIN
+			}
+		};
+								
+		$scope.map.markers = [
+		{
+			id : 1,
+			latitude: 43.67023,
+			longitude: -79.38676
+	    },
+	    {
+			id : 2,
+			latitude: 43.67023,
+			longitude: -81.38676
+	    },
+	    {
+			id : 3,
+			latitude: 43.67023,
+			longitude: -80.38676
+	    }
+	    ];
+	    
+	    uiGmapGoogleMapApi.then(function(maps) {
+			var input = document.getElementById('searchTextField');
+			var options = {types: ['(cities)']};
+			var map = $scope.map;
+		
+			var autocomplete = new google.maps.places.Autocomplete(input, options);	
+	    });
+	    
+	    // hotel list call, expedia
+		$.ajax({
+			type: 'GET',
+			url: 'https://book.api.ean.com/ean-services/rs/hotel/v3/list?locale=en_US&destinationString=las vegas,nv&apiKey=70303auc6h8hqutunreio3u8pl&minorRev=99&departureDate=10/20/15&room=2&arrivalDate=10/10/2015&curencyCode=USD&cid=55505',
+			async: false,
+			contentType: "application/json",
+			dataType: 'jsonp',
+			
+			success: function(data){
+				console.log(data);
+			},
+			
+			error: function(e){
+				console.log(e.message);
+			}
+		});
+	   	
+			
 
         $scope.showSelected = function(input) {
           //console.log(input)
